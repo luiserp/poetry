165cf539579be8a34d085424e61d6fa72eacf641
luiserp-tx <luisreyesperez98@gmail.com>
2023-11-22 15:41:16 -0500

    bg

--- components/Core/Dropdown.vue
+++ components/Core/Dropdown.vue
@@ -1,10 +1,10 @@
 <template>
   <Listbox as="div" v-model="selected" class="inline-block text-white">
     <div class="relative mt-2">
-      <ListboxButton class="relative w-full cursor-default rounded-md bg-blue-500 py-1.5 pl-3 pr-10 text-left shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+      <ListboxButton class="relative w-full cursor-default rounded-md bg-blue-500 py-1.5 pl-3 pr-10 text-left shadow-sm ring-1 ring-inset ring-white focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
         <span class="block truncate">{{ selected?.name ?? "selecciona" }}</span>
         <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-          <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+          <ChevronUpDownIcon class="h-5 w-5 text-white-400" aria-hidden="true" />
         </span>
       </ListboxButton>
 
