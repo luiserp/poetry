fa6171ad0ba0d5fc6be12ad9c18aa32f44e28929
luiserp <luisreyesperez98@gmail.com>
2023-12-12 14:20:17 -0500

    tlon

--- pages/tlon/index.vue
+++ pages/tlon/index.vue
@@ -4,6 +4,9 @@
       class="container px-10 mt-6 text-justify leading-8 text-gray-900 p-10 border-solid border bg-gray-50 shadow-sm rounded-lg mx-4 w-[90%] min-h-screen space-y-4"
       data-aos="fade-up"
     >
+      <h2 class="text-lg font-semibold">Tlön, Uqbar, Orbis Tertius</h2>
+      <h4 class="font-semibold">I</h4>
+
       <p class="select-none text-lg leading-10">
         Debo a la conjunción de un espejo y de una enciclopedia el
         descubrimiento de Uqbar. El espejo inquietaba el fondo de un corredor en
@@ -27,7 +30,8 @@
             { id: 1, name: 'terrible' },
           ]"
         />. Entonces Bioy Casares recordó que uno de los heresiarcas de Uqbar
-        había declarado que los espejos y la cópula son <Dropdown
+        había declarado que los espejos y la cópula son
+        <Dropdown
           :options="[
             { id: 1, name: 'abominables' },
             { id: 1, name: 'todopoderosos' },
@@ -35,16 +39,15 @@
             { id: 1, name: 'despreciables' },
             { id: 1, name: 'invaluables' },
           ]"
-        />, porque
-        multiplican el número de los hombres. Le pregunté el origen de esa
-        memorable sentencia y me contestó que The Anglo-American Cyclopaedia la
-        registraba, en su artículo sobre Uqbar. La quinta (que habíamos
-        alquilado amueblada) poseía un ejemplar de esa obra. En las últimas
-        páginas del volumen XLVI dimos con un artículo sobre Upsala; en las
-        primeras del XLVII, con uno sobre Ural-Altaic Languages, pero ni una
+        />, porque multiplican el número de los hombres. Le pregunté el origen
+        de esa memorable sentencia y me contestó que The Anglo-American
+        Cyclopaedia la registraba, en su artículo sobre Uqbar. La quinta (que
+        habíamos alquilado amueblada) poseía un ejemplar de esa obra. En las
+        últimas páginas del volumen XLVI dimos con un artículo sobre Upsala; en
+        las primeras del XLVII, con uno sobre Ural-Altaic Languages, pero ni una
         palabra sobre Uqbar. Bioy, un poco azorado, interrogó los tomos del
         índice. Agotó en vano todas las lecciones imaginables: Ukbar, Ucbar,
-        Ookbar, Oukbahr... Antes de irse, me dijo que era una región del 
+        Ookbar, Oukbahr... Antes de irse, me dijo que era una región del
         <Dropdown
           :options="[
             { id: 1, name: 'Irak o del Asia Menor' },
@@ -54,18 +57,18 @@
             { id: 1, name: 'Islandia' },
           ]"
         />
-        . Confieso que asentí con alguna incomodidad. Conjeturé
-        que ese país indocumentado y ese heresiarca anónimo eran una ficción
-        improvisada por la modestia de Bioy para justificar una frase. El examen
-        estéril de uno de los atlas de Justus Perthes fortaleció mi duda. Al día
-        siguiente, Bioy me llamó desde Buenos Aires. Me dijo que tenía a la
-        vista el artículo sobre Uqbar, en el volumen XXVI de la Enciclopedia. No
-        constaba el nombre del heresiarca, pero sí la noticia de su doctrina,
-        formulada en palabras casi idénticas a las repetidas por él, aunque -tal
-        vez- literariamente inferiores. Él había recordado: Copulation and
-        mirrors are abominable. El texto de la Enciclopedia decía: Para uno de
-        esos gnósticos, el visible universo era una ilusión o (más precisamente)
-        un sofisma. Los espejos y la paternidad son abominables (mirrors and
+        . Confieso que asentí con alguna incomodidad. Conjeturé que ese país
+        indocumentado y ese heresiarca anónimo eran una ficción improvisada por
+        la modestia de Bioy para justificar una frase. El examen estéril de uno
+        de los atlas de Justus Perthes fortaleció mi duda. Al día siguiente,
+        Bioy me llamó desde Buenos Aires. Me dijo que tenía a la vista el
+        artículo sobre Uqbar, en el volumen XXVI de la Enciclopedia. No constaba
+        el nombre del heresiarca, pero sí la noticia de su doctrina, formulada
+        en palabras casi idénticas a las repetidas por él, aunque -tal vez-
+        literariamente inferiores. Él había recordado: Copulation and mirrors
+        are abominable. El texto de la Enciclopedia decía: Para uno de esos
+        gnósticos, el visible universo era una ilusión o (más precisamente) un
+        sofisma. Los espejos y la paternidad son abominables (mirrors and
         fatherhood are hateful) porque lo multiplican y lo divulgan. Le dije,
         sin faltar a la verdad, que me gustaría ver ese artículo. A los pocos
         días lo trajo. Lo cual me sorprendió, porque los escrupulosas índices
@@ -92,39 +95,39 @@
         fronteras de Uqbar, pero sus nebulosos puntos de referencias eran ríos y
         cráteres y cadenas de esa misma región. Leímos, verbigracia, que las
         tierras bajas de Tsai Jaldún y el delta del Axa definen la frontera del
-        sur y que en las islas de ese delta procrean <Dropdown
+        sur y que en las islas de ese delta procrean
+        <Dropdown
           :options="[
             { id: 1, name: 'los caballos salvajes' },
             { id: 1, name: 'bestias de seis patas con rostros humanos' },
             { id: 1, name: 'caballos con escamas' },
             { id: 1, name: 'seres amorfos y translúcidos' },
           ]"
-        />__.
-        Eso, al principio de la página 918. En la sección histórica (página 920)
-        supimos que a raíz de las persecuciones religiosas del siglo trece, los
-        ortodoxos buscaron amparo en las islas, donde perduran todavía sus
-        obeliscos y donde no es raro exhumar sus espejos de piedra. La sección
-        idioma y literatura era breve. Un solo rasgo memorable: anotaba que la
-        literatura de Uqbar era de carácter
+        />__. Eso, al principio de la página 918. En la sección histórica
+        (página 920) supimos que a raíz de las persecuciones religiosas del
+        siglo trece, los ortodoxos buscaron amparo en las islas, donde perduran
+        todavía sus obeliscos y donde no es raro exhumar sus espejos de piedra.
+        La sección idioma y literatura era breve. Un solo rasgo memorable:
+        anotaba que la literatura de Uqbar era de carácter
         <Dropdown
           :options="[
             { id: 1, name: 'fantástico' },
             { id: 1, name: 'verídico' },
             { id: 1, name: 'real' },
           ]"
-        />  y que sus epopeyas y sus
-        leyendas no se referían jamás a la realidad, sino a las dos regiones
-        imaginarias de Mlejnas y de Tlön... La bibliografía enumeraba cuatro
-        volúmenes que no hemos encontrado hasta ahora, aunque el tercero - Silas
-        Haslam: History of the Land Called Uqbar, 1874-figura en los catálogos
-        de librería de Bernard Quaritch.1 El primero, Lesbare und lesenswerthe
-        Bemerkungen über das Land Ukkbar in Klein-Asien, data de 1641 y es obra
-        de Johannes Valentinus Andreä. El hecho es significativo; un par de años
-        después, di con ese nombre en las inesperadas páginas de De Quincey
-        (Writings, decimotercero volumen) y supe que era el de un teólogo alemán
-        que a principios del siglo XVII describió la imaginaria comunidad de la
-        Rosa-Cruz -que otros luego fundaron, a imitación de lo prefigurado por
-        él.
+        />
+        y que sus epopeyas y sus leyendas no se referían jamás a la realidad,
+        sino a las dos regiones imaginarias de Mlejnas y de Tlön... La
+        bibliografía enumeraba cuatro volúmenes que no hemos encontrado hasta
+        ahora, aunque el tercero - Silas Haslam: History of the Land Called
+        Uqbar, 1874-figura en los catálogos de librería de Bernard Quaritch.1 El
+        primero, Lesbare und lesenswerthe Bemerkungen über das Land Ukkbar in
+        Klein-Asien, data de 1641 y es obra de Johannes Valentinus Andreä. El
+        hecho es significativo; un par de años después, di con ese nombre en las
+        inesperadas páginas de De Quincey (Writings, decimotercero volumen) y
+        supe que era el de un teólogo alemán que a principios del siglo XVII
+        describió la imaginaria comunidad de la Rosa-Cruz -que otros luego
+        fundaron, a imitación de lo prefigurado por él.
         <br />
         Esa noche visitamos la Biblioteca Nacional. En vano fatigamos atlas,
         catálogos, anuarios de sociedades geográficas, memorias de viajeros e
@@ -134,6 +137,166 @@
         librería de Corrientes y Talcahuano los negros y dorados lomos de la
         Anglo-American Cyclopaedía... Entró e interrogó el volumen XXVI.
         Naturalmente, no dio con el menor indicio de Uqbar.
+      </p>
+
+      <h4 class="font-semibold">II</h4>
+
+      <p class="select-none text-lg leading-10">
+        Algún recuerdo limitado y menguante de Herbert Ashe, ingeniero de los
+        ferrocarriles del Sur, persiste en el hotel de Adrogué, entre las
+        efusivas madreselvas y en el fondo ilusorio de los espejos. En vida
+        padeció de irrealidad, como tantos ingleses; muerto, no es siquiera el
+        fantasma que ya era entonces. Era alto y desganado y su cansada barba
+        rectangular había sido roja. Entiendo que era viudo, sin hijos. Cada
+        tantos años iba a Inglaterra: a visitar (juzgo por unas fotografías que
+        nos mostró) un reloj de sol y unos robles. Mi padre había estrechado con
+        él (el verbo es excesivo) una de esas amistades inglesas que empiezan
+        por excluir la confidencia y que muy pronto omiten el diálogo. Solían
+        ejercer un intercambio de libros y de periódicos; solían batirse al
+        ajedrez, taciturnamente... Lo recuerdo en el corredor del hotel, con un
+        libro de matemáticas en la mano, mirando a veces los colores
+        irrecuperables del cielo. Una tarde, hablamos del sistema duodecimal de
+        numeración (en el que doce se escribe 10). Ashe dijo que precisamente
+        estaba trasladando no sé qué tablas duodecimales a sexagesimales (en las
+        que sesenta se escribe 10). Agregó que ese trabajo le había sido
+        encargado por un noruego: en Rio Grande do Sul. Ocho años que lo
+        conocíamos y no había mencionado nunca su estadía en esa región...
+        Hablamos de vida pastoril, de capangas, de la etimología brasilera de la
+        palabra gaucho (que algunos viejos orientales todavía pronuncian gaúcho)
+        y nada más se dijo -Dios me perdone- de funciones duodecimales. En
+        setiembre de 1937 (no estábamos nosotros en el hotel) Herbert Ashe murió
+        de la rotura de un aneurisma. Días antes, había recibido del Brasil un
+        paquete sellado y certificado. Era un libro en octavo mayor. Ashe lo
+        dejó en el bar, donde -meses después lo encontré. Me puse a hojearlo y
+        sentí un vértigo asombrado y ligero que no describiré, porque ésta no es
+        la historia de mis emociones sino de Uqbar y Tlön y Orbis Tertius. En
+        una noche del Islam que se llama la Noche de las Noches se abren de par
+        en par las secretas puertas del cielo y es más dulce el agua en los
+        cántaros; si esas puertas se abrieran, no sentiría lo que en esa tarde
+        sentí. El libro estaba redactado en inglés y lo integraban 1001 páginas.
+        En el amarillo lomo de cuero leí estas curiosas palabras que la falsa
+        carátula repetía: A First Encyclopaedia of Tlön. vol. XI. Hlaer to
+        Jangr. No había indicación de fecha ni de lugar. En la primera página y
+        en una hoja de papel de seda que cubría una de las láminas en colores
+        había estampado un óvalo azul con esta inscripción: Orbis Tertius. Hacía
+        dos años que yo había descubierto en un tomo de cierta enciclopedia
+        práctica una somera descripción de un falso país; ahora me deparaba el
+        azar algo más precioso y más arduo. Ahora tenía en las manos un vasto
+        fragmento metódico de la historia total de un planeta desconocido, con
+        sus arquitecturas y sus barajas, con el pavor de sus mitologías y el
+        rumor de sus lenguas, con sus emperadores y sus mares, con sus minerales
+        y sus pájaros y sus peces, con su álgebra y su fuego, con su
+        controversia teológica y metafísica. Todo ello articulado, coherente,
+        sin visible propósito doctrinal o tono paródico.
+        <br />
+        En el "onceno tomo" de que hablo hay alusiones a tomos ulteriores y
+        precedentes. Néstor Ibarra, en un artículo ya clásico de la N. R. F., ha
+        negado que existen esos aláteres; Ezequiel Martínez Estrada y Drieu La
+        Rochelle han refutado, quizá victoriosamente, esa duda. El hecho es que
+        hasta ahora las pesquisas más diligentes han sido estériles. En vano
+        hemos desordenado las bibliotecas de las dos Américas y de Europa.
+        Alfonso Reyes, harto de esas fatigas subalternas de índole policial,
+        propone que entre todos acometamos la obra de reconstruir los muchos y
+        macizos tomos que faltan: ex ungue leonem. Calcula, entre veras y
+        burlas, que una generación de tlönistas puede bastar. Ese arriesgado
+        cómputo nos retrae al problema fundamental: ¿Quiénes inventaron a Tlön?
+        El plural es inevitable, porque la hipótesis de un solo inventor -de un
+        infinito Leibniz obrando en la tiniebla y en la modestia- ha sido
+        descartada unánimemente. Se conjetura que este brave new world es obra
+        de una sociedad secreta de astrónomos, de biólogos, de ingenieros, de
+        metafísicos, de poetas, de químicos, de algebristas, de moralistas, de
+        pintores, de geómetras... dirigidos por un oscuro hombre de genio.
+        Abundan individuos que dominan esas disciplinas diversas, pero no los
+        capaces de invención y menos los capaces de subordinar la invención a un
+        riguroso plan sistemático. Ese plan es tan vasto que la contribución de
+        cada escritor es infinitesimal. Al principio se creyó que Tlön era un
+        mero caos, una irresponsable licencia de la imaginación; ahora se sabe
+        que es un cosmos y las íntimas leyes que lo rigen han sido formuladas,
+        siquiera en modo provisional.Básteme recordar que las contradicciones
+        aparentes del Onceno Tomo son la piedra fundamental de la prueba de que
+        existen los otros: tan lúcido y tan justo es el orden que se ha
+        observado en él. Las revistas populares han divulgado, con perdonable
+        exceso, la zoología y la topografía de Tlön; yo pienso que sus
+        <Dropdown
+          :options="[
+            { id: 1, name: 'tigres transparentes' },
+            { id: 2, name: 'unicornios amarillos' },
+            { id: 3, name: 'lagartos gigantes' },
+            { id: 4, name: 'diminutos elefantes verdes' },
+          ]"
+        />
+        y sus torres de
+        <Dropdown
+          :options="[
+            { id: 1, name: 'sangre' },
+            { id: 2, name: 'piedra translúcida ' },
+            { id: 3, name: 'espuma rosa' },
+            { id: 4, name: 'lava azul' },
+          ]"
+        />
+        no merecen, tal vez, la continua atención de todos los hombres. Yo me
+        atrevo a pedir unos minutos para su concepto del universo. Hume notó
+        para siempre que los argumentos de Berkeley no admiten la menor réplica
+        y no causan la menor convicción. Ese dictamen es del todo verídico en su
+        aplicación a la tierra; del todo falso en Tlön. Las naciones de ese
+        planeta son -congénitamente- idealistas. Su lenguaje y las derivaciones
+        de su lenguaje -la religión, las letras, la metafísica- presuponen el
+        idealismo. El mundo para ellos no es un concurso de objetos en el
+        espacio; es una serie heterogénea de actos independientes. Es sucesivo,
+        temporal, no espacial. No hay sustantivos en la conjetural Ursprache de
+        Tlön, de la que proceden los idiomas "actuales" y los dialectos: hay
+        verbos impersonales, calificados por sufijos (o prefijos) monosilábicos
+        de valor adverbial. Por ejemplo: no hay palabra que corresponda a la
+        palabra luna, pero hay un verbo que sería en español lunecer o lunar.
+        Surgió la luna sobre el río se dice hlör u fang axaxaxas mlö o sea en su
+        orden: hacia arriba (upward) detrás duradero-fluir luneció. (Xul Solar
+        traduce con brevedad: upa tras perfluyue lunó. Upward, behind the
+        onstreaming it mooned...
+
+        <br />
+        Aquí doy término a la parte personal de mi narración. Lo demás está en
+        la memoria (cuando no en la esperanza o en el temor) de todos mis
+        lectores. Básteme recordar o mencionar los hechos subsiguientes, con una
+        mera brevedad de palabras que el cóncavo recuerdo general enriquecerá o
+        ampliará. Hacia 1944 un investigador del diario The American (de
+        Nashville, Tennessee) exhumó en una biblioteca de Memphis los cuarenta
+        volúmenes de la Primera Enciclopedia de Tlön. Hasta el día de hoy se
+        discute si ese descubrimiento fue casual o si lo consintieron los
+        directores del todavía nebuloso Orbís Tertius. Es verosímil lo segundo.
+        Algunos rasgos increíbles del Onceno Tomo (verbigracia, la
+        multiplicación de los hrönir) han sido eliminados o atenuados en el
+        ejemplar de Memphis; es razonable imaginar que esas tachaduras obedecen
+        al plan de exhibir un mundo que no sea demasiado incompatible con el
+        mundo real. La diseminación de objetos de Tlön en diversos países
+        complementaría ese plan...6 El hecho es que la prensa internacional
+        voceó infinitamente el "hallazgo". Manuales, antologías, resúmenes,
+        versiones literales, reimpresiones autorizadas y reimpresiones piráticas
+        de la Obra Mayor de los Hombres abarrotaron y siguen abarrotando la
+        tierra. Casi inmediatamente, la realidad cedió en más de un punto. Lo
+        cierto es que anhelaba ceder. Hace diez años bastaba cualquier simetría
+        con apariencia de orden -el materialismo dialéctico, el antisemitismo,
+        el nazismo- para embelesar a los hombres. ¿Cómo no someterse a Tlön, a
+        la minuciosa y vasta evidencia de un planeta ordenado? Inútil responder
+        que la realidad también está ordenada. Quizá lo esté, pero de acuerdo a
+        leyes divinas -traduzco: a leyes inhumanas- que no acabamos nunca de
+        percibir. Tlön será un laberinto, pero es un laberinto urdido por
+        hombres, un laberinto destinado a que lo descifren los hombres. El
+        contacto y el hábito de Tlön han desintegrado este mundo. Encantada por
+        su rigor, la humanidad olvida y torna a olvidar que es un rigor de
+        ajedrecistas, no de ángeles. Ya ha penetrado en las escuelas el
+        (conjetural), "idioma primitivo" de Tlön; ya la enseñanza de su historia
+        armoniosa (y llena de episodios conmovedores) ha obliterado a la que
+        presidió mi niñez; ya en las memorias un pasado ficticio ocupa el sitio
+        de otro, del que nada sabemos con certidumbre -ni siquiera que es falso.
+        Han sido reformadas la numismática, la farmacología y la arqueología.
+        Entiendo que la biología y las matemáticas aguardan también su avatar...
+        Una dispersa dinastía de solitarios ha cambiado la faz del mundo. Su
+        tarea prosigue. Si nuestras previsiones no erran, de aquí a cien años
+        alguien descubrirá los cien tomos de la Segunda Enciclopedia de Tlön.
+        Entonces desaparecerán del planeta el inglés y el francés y el mero
+        español. El mundo será Tlön. Yo no hago caso, yo sigo revisando en los
+        quietos días del hotel de Adrogué una indecisa traducción quevediana
+        (que no pienso dar a la imprenta) del Urn Burial de Browne.
       </p>
 
       <div class="flex space-x-4 justify-center">
